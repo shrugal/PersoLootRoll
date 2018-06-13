0730fc5f56f34795496ac01c041af0d592a39ba0
Nikolas Honnef <nhonnef@gmail.com>
2018-06-13 14:06:35 +0200

    Version bump to 5

--- Init.lua
+++ Init.lua
@@ -3,7 +3,7 @@ LibStub("AceAddon-3.0"):NewAddon(Addon, Name, "AceConsole-3.0", "AceComm-3.0", "
 
 -- Constants
 Addon.ABBR = "PLR"
-Addon.VERSION = 4
+Addon.VERSION = 5
 Addon.DEBUG = false
 
 -- Modules
