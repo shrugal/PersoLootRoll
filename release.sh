02d5ace2208fa2fb52533ae3037698eaa7a6d993
Nikolas Honnef <nhonnef@gmail.com>
2019-01-16 23:56:40 +0100

    Updated release script to handle wowinterface beta releases

--- release.sh
+++ release.sh
@@ -4,4 +4,9 @@ if [ -f ".release/.env" ]; then
 	source ".release/.env"
 fi
 
+tag=$(git describe --tags)
+if [[ -z "$tag" || ! ( "$tag" =~ ^v?[0-9][0-9.]*$ || "${tag,,}" == *"release"* ) ]]; then
+	WOWI_ID=$WOWI_ID_BETA
+fi
+
 .release/release.sh -p $CF_ID -w $WOWI_ID "$@"
